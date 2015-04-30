59858f915818d1a6e7ade0e74fe7fb342547bf30
sumitpaulsmx <sumitpaulsmx@gmail.com>
2015-04-30 19:08:14 +0200

    Fitxers php de paisos

---
+++ be.php
@@ -0,0 +1,58 @@
+<?php
+
+?>
+<html>
+<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/css.css">
+		<script src="js/jquery.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	</head>
+<body>
+	<div class="container">
+		<div class="row">
+	<div class="btn-toolbar">
+	  <div class="btn-group">
+	    <a href="#" class="btn btn-default">1990</a>
+	    <a href="#" class="btn btn-default">2000</a>
+	    <a href="#" class="btn btn-default">2005</a>
+	    <a href="#" class="btn btn-default">3005</a>
+	     <a href="#" class="btn btn-default">1990</a>
+	    <a href="#" class="btn btn-default">2000</a>
+	    <a href="#" class="btn btn-default">2005</a>
+	    <a href="#" class="btn btn-default">3005</a>
+	     <a href="#" class="btn btn-default">1990</a>
+	    <a href="#" class="btn btn-default">2000</a>
+	    <a href="#" class="btn btn-default">2005</a>
+	    <a href="#" class="btn btn-default">3005</a>
+	     <a href="#" class="btn btn-default">1990</a>
+	    <a href="#" class="btn btn-default">2000</a>
+	    <a href="#" class="btn btn-default">2005</a>
+	    <a href="#" class="btn btn-default">3005</a>
+	     <a href="#" class="btn btn-default">1990</a>
+	    <a href="#" class="btn btn-default">2000</a>
+	    <a href="#" class="btn btn-default">2005</a>
+	    <a href="#" class="btn btn-default">3005</a>
+	     <a href="#" class="btn btn-default">1990</a>
+	    <a href="#" class="btn btn-default">2000</a>
+	    <a href="#" class="btn btn-default">2005</a>
+	    <a href="#" class="btn btn-default">3005</a>
+	    <a href="#" class="btn btn-default">2000</a>
+	    <a href="#" class="btn btn-default">2005</a>
+	    <a href="#" class="btn btn-default">3005</a>
+	    <a href="#" class="btn btn-default">2000</a>
+	    <a href="#" class="btn btn-default">2005</a>
+	    <a href="#" class="btn btn-default">3005</a>
+	    <a href="#" class="btn btn-default">2000</a>
+	    <a href="#" class="btn btn-default">2005</a>
+	    <a href="#" class="btn btn-default">3005</a> 
+	    <a href="#" class="btn btn-default">2000</a>
+	    <a href="#" class="btn btn-default">2005</a>
+	    <a href="#" class="btn btn-default">3005</a>
+	  </div>
+	</div>
+	</div>
+	</div>
+</body>
+</html>
